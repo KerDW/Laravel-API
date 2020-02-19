d4dccc0850d980b1c16440bd7e761796082902ca
Álvaro Zamora <alvarozajim@gmail.com>
2020-02-19 22:27:25 +0100

    install dependencies and migration

--- routes/api.php
+++ routes/api.php
@@ -22,6 +22,6 @@ Route::middleware('auth:api')->get('/user', function (Request $request) {
 // Route::post('/cars', 'CarController@store');
 // Route::put('/cars/{car}', 'CarController@update');
 
-// these two ^ v should be the same but after using apiResource individual routes should be added above apiResource to avoid conflict
+// these two ^ v are the same but using apiResource means that individual routes should be added above it to avoid possible conflicts
 
 Route::apiResource('cars', 'CarController');
